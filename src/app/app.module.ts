c26a2d6c69f95b1daa83bdf4cfb22029330ab6e0
Kuroky360 <kuroky360.cn@gmail.com>
2017-07-16 12:19:16 +0800

    Add the ability to search by name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroDetailComponent} from './hero-detail.component';
 import { HeroesComponent} from './heroes.component'
 import { HeroService} from "./hero.service";
 import { DashboardComponent} from './dashboard.componet';
+import { HeroSearchComponent} from './hero-search.component';
 
 import { AppRoutingModule} from './app-routing.module';
 
@@ -19,7 +20,8 @@ import { InMemoryDataService } from './in-memory-data.service';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero-search.service.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by Kuroky360 on 7/16/17.
+ */
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Hero} from './hero';
+
+@Injectable()
+export class HeroSearchService{
+    constructor(private http:Http){};
+    search(term:string):Observable<Hero[]>{
+        return this.http.get(`api/heroes/?name=${term}`)
+            .map(response=>response.json().data as Hero[]);
+    }
+}
